4f3c6922d0000db8353ae1c8461a933c71f4d29b
Oleh_Denysenko <Oleh.Denysenkoo@gmail.com>
2025-01-27 10:45:38 +0200

    finished hw 4

--- js/task-3.js
+++ js/task-3.js
@@ -1,14 +1,24 @@
+const profile = {
+  username: "Jacob",
+    playTime: 300,
+    
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  }
+};
 
 
-function getElementWidth(content, padding, border) {
+console.log(profile.getInfo()); 
 
-    return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
-}
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); 
 
-
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
-
-
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); 
